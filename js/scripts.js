341d56ba50a8fe7d6084f4f9e033d2383eacb80a
Jack <skelton.jt9@gmail.com>
2021-11-07 14:48:51 -0500

    added list elements and retuned arrays to list
    Co-authored-by: Matthew Banse-Fay <matthew.bansefay@gmail.com>
    Co-authored-by: RosarioRu <RosarioRu@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,19 @@ $(document).ready(function() {
     $("input#answer4").val(),
     $("input#answer5").val()];
     console.log(favoriteArray);
+
+    console.log(favoriteArray[0], favoriteArray[1], favoriteArray[2]);
+    
+    let newArray = [];
+    newArray.push(favoriteArray[0]);
+    newArray.push(favoriteArray[1]);
+    newArray.push(favoriteArray[2]);
+
+    console.log(newArray);
+  
+    $("#result1").text(newArray[0]);
+    $("#result2").text(newArray[1]);
+    $("#result3").text(newArray[2]);
   });
 });
 
