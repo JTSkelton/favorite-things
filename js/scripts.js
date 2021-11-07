b504461667ae38d44fc7976417e3dcb663346c28
Jack <skelton.jt9@gmail.com>
2021-11-07 14:15:33 -0500

    added array inputs in js
    Co-authored-by: Matthew Banse-Fay <matthew.bansefay@gmail.com>
    Co-authored-by: RosarioRu <RosarioRu@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+  $("#favorite-form").submit(function(event) {
+    event.preventDefault();
+    const favoriteArray = [$("input#answer1").val(), 
+    $("input#answer2").val(),
+    $("input#answer3").val(),
+    $("input#answer4").val(),
+    $("input#answer5").val()];
+    console.log(favoriteArray);
+  });
 });
+
+
